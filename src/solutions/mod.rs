01b20fc75428a7de82e7d8a3750c9f0f295e326a
Virus <zxcv.fghj.uiop@gmail.com>
2022-08-19 13:36:28 +0900

    0009_palindrome_number

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -3,3 +3,4 @@ mod s0002_add_two_numbers;
 mod s0003_longest_substring_without_repeating_characters;
 mod s0005_longest_palindromic_substring;
 mod s0007_reverse_integer;
+mod s0009_palindrome_number;
